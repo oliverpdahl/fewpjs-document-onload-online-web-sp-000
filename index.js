6999d5132b9362b7b6e8d767d934fd6562de878a
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 19:05:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   let text = document.getElementById('text')
-  text.textContent = 'This is realy cool!'
+  text.textContent = 'This is really cool!'
 });
- 
